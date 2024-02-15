968b6263c338913a2d9e9172e94f3a3929c3253d
samue <samuel.jdev11@gmail.com>
2024-02-15 18:51:45 -0400

    01-primeiro componente

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,26 @@ function App() {
   return (
     <>
       <h1>Primeiro componente</h1>
+      <Aluno nome='Samuel' idade={34} /> {/* inovando componente aluno e passando propriedades */}
+      <Aluno nome='Lucia' idade={34} />  {/* reaproveitando componente */}
+      <Aluno nome='Karine' idade={34} /> {/* reaproveitando componente */}
     </>
+  )
+}
+
+// tipando componente aluno atraves da interface
+interface AlunoProps {
+  nome: string;
+  idade: number;
+}
+
+// criando componente aluno, que espera receber duas propriedades
+function Aluno({ nome, idade }: AlunoProps) {
+  return (
+    <div>
+      <h1>Aluno: {nome}</h1>
+      <h3>Idade: {idade}</h3>
+    </div>
   )
 }
 
